424a4cb05487ef074c09088bc478ba0d315d33fd
LoganJeong0 <loganjeong0@gmail.com>
2024-09-10 22:01:07 +0000

    commit1

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Hello world!";
+        boolean b = true;
+        int i = 23;
+        double d = 2.4;
+        double addition = i + d;
+        System.out.println(greeting);
+        System.out.println(i + " + " + d + " is equal to " + addition);
+        System.out.println(b);
+    }
+}
